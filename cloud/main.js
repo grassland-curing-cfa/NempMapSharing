2941d1f47b48362bf6cb3cabf6e1e18feec8c3b7
A.C <grassland.curing.cfa@gmail.com>
2017-01-30 15:33:00 +1100

    Updated package.json and index.js for initial checkin

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,16 @@
+var _ = require('underscore');
+var moment = require('moment');
+
+var APP_ID = process.env.APP_ID;
+var MASTER_KEY = process.env.MASTER_KEY;
+var SERVER_URL = process.env.SERVER_URL;
+var APP_NAME = process.env.APP_NAME;
+
+var MG_DOMAIN = process.env.MG_DOMAIN;
+var MG_KEY = process.env.MG_KEY;
+
+var CFA_NEMP_EMAIL = process.env.EMAIL_ADDR_CFA_NEMP;
 
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+  response.success("Hello world from " + APP_NAME);
 });
