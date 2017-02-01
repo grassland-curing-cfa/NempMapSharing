7425cc7d5b5de31f4336995bcaad9dda62d08f13
A.C <grassland.curing.cfa@gmail.com>
2017-02-01 14:23:52 +1100

    Updated main.js by adding sendMapSharingEmailToUsers cloud functiom

--- cloud/main.js
+++ cloud/main.js
@@ -55,3 +55,82 @@ Parse.Cloud.beforeDelete("MAPSHARING_MAP", function(request, response) {
 		response.success();
 	}
 });
+
+//Send an email for NEMP MapSharing product to the State Control Team mail list.
+Parse.Cloud.define("sendMapSharingEmailToUsers", function(request, response) {
+	var rawStrToday = request.params.dateString;	// in the format of "%d_%m_%Y"
+	var strToday = rawStrToday.replace(/_/g, '/');
+	
+	var queryMapSharingClass = new Parse.Query("MAPSHARING_MAP");
+	queryMapSharingClass.descending("createdAt");
+	queryMapSharingClass.find().then(function(results) {
+		// results is array of MAPSHARING_MAP records
+		// We only care about the most recent one
+		var latestUploaded = results[0];
+		var uploadedUrl = latestUploaded.get("MapSharingFileUrl");
+		
+		console.log("The most recent MapSharing map uploaded file Url: " + uploadedUrl)
+		
+		// use Mailgun to send email
+		var mailgun = require('mailgun-js')({apiKey: MG_KEY, domain: MG_DOMAIN});
+		
+		var htmlString = 
+					'<!DOCTYPE html><html>' + 
+					'<head>' + 
+					'<title>MapSharing product</title>' + 
+					'<style>' + 
+					'p, li {margin:0cm; margin-bottom:.0001pt; font-size:11.0pt; font-family:"Calibri","sans-serif";}' + 
+					'</style>' + 
+					'</head>' + 
+					'<body>' + 
+					//'<p>Hello %recipient%,</p>' + 
+					'<p>Hello All,</p>' + 
+					'<br>' + 
+					'<p>The MapSharing map has been updated ' + strToday + '. To view the report, please click the link <a href="' + uploadedUrl + '" target="_top">here</a>.</p>' + 
+					'<br>' + 
+					'<p>Please note: If the report does not display properly in your web browser, please save it to your desktop and view directly using Acrobat Reader.</p>' + 
+					'<br>' + 
+					'<p>Kind Regards,</p>' + 
+					'<p>The NEMP Grassland Curing Team</p>' + 
+					'<br>' + 
+					'<table><tr><td width="25%"><img src="http://www.cfa.vic.gov.au/img/logo.png" width="64" height="64" alt="CFA_LOGO" /></td>' + 
+					'<td><p style="color:#C00000; font-weight: bold;">NEMP Grassland Curing Team</p><p>CFA HQ - Fire & Emergency Management</p><p>8 Lakeside Drive, Burwood East, Victoria 3151</p>' + 
+					'<p>E: <a href="mailto:' + CFA_NEMP_EMAIL + '" target="_top">' + CFA_NEMP_EMAIL + '</a></p></td></tr></table>' + 
+					'<br>' + 
+					'<p><i>Note: This email has been automatically generated. Please do not reply to this email.</i></p>' + 
+					'</body>' + 
+					'</html>';
+		
+		var result = null;
+		
+		//
+		var data = {
+			to: "a.chen@cfa.vic.gov.au",
+			//cc: CFA_NEMP_EMAIL,
+			from: CFA_NEMP_EMAIL,
+			subject: "Grassland Curing Map Sharing - " + strToday,
+			text: "",
+			html: htmlString
+		};
+
+		mailgun.messages().send(data, function (error, body) {
+			if (error) {
+				console.log(error);
+				result = {
+					"result": false,
+					"details": error
+				};
+				response.error(result);
+			} else {
+				console.log(body);
+				result = {
+					"result": true,
+					"details": JSON.stringify(body)
+				};
+				response.success(result);
+			}
+		});
+	}, function(error) {
+	    response.error("MAPSHARING_MAP table lookup failed");
+	});
+});
